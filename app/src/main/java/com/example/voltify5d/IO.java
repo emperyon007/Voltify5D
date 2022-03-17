1dbdaf671a5c09e7ccde67619b3204160b19488f
emperyon007 <lorenzopapini232@gmail.com>
2022-03-17 11:51:06 +0100

    Fixen JSONread method with arrayAdapter refresh

--- app/src/main/java/com/example/voltify5d/IO.java
+++ app/src/main/java/com/example/voltify5d/IO.java
@@ -143,6 +143,8 @@ public class IO
                     );
             //Log.d("BraniObj", jsonObjectB.getString("Titolo"));
             Memory.brani.add(toAppend.toString());
+
+
         }
 
 
--- app/src/main/java/com/example/voltify5d/MainActivity2.java
+++ app/src/main/java/com/example/voltify5d/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -111,6 +112,8 @@ public class MainActivity2 extends AppCompatActivity {
                 }
 
                 Log.d("printed", fileText);
+
+                arrayAdapter.notifyDataSetChanged();
             }
 
         });
